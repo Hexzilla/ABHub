41df9c935a9299a32c4c0d1b7d3e6b37c4e78467
goldencat-tom <bluebird6922@gmail.com>
2021-08-20 06:10:46 +0300

    add user model

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,18 @@
-import {Router, Request, Response} from "express";
+import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { User } from "../models/user";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", async (req: Request, res: Response) => {
+  let user: User = {
+    email: "bluebird2511@prisma.io",
+    name: "Blue Bird",
+  };
+  const createUser = await prisma.user.create({ data: user });
+  console.log("createUser", createUser);
+
   res.send("Well done!!");
 });
 

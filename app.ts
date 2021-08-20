eda97156208c18ea90d2a20d0cf882c12049474a
goldencat-tom <bluebird6922@gmail.com>
2021-08-20 04:59:05 +0300

    add @type/morgan, error handlers

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import http from "http";
 import express from "express";
+import {Request, Response} from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import methodOverride from "method-override";
 import morgan from "morgan";
 
+const isProduction = process.env.NODE_ENV === "production";
 const app = express();
 
 app.use(cors());
@@ -16,6 +18,42 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.send("Well done!");
+});
+
+
+/// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: Function) => {
+  const err = new Error("Not Found");
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use((err: any, req: Request, res: Response) => {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err: any, req: Request, res: Response) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
 });
 
 // finally, let's start our server...
